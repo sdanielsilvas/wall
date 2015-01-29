82e4648247f36a114e6fe9a49d11ea0625b131a8
System User <sdanielsilvas@gmail.com>
2015-01-28 19:22:24 -0500

    init repository

---
+++ publicationsController.js
@@ -0,0 +1,7 @@
+function publicationController($scope) {
+    $scope.publications=[
+		{name:'Jani',country:'Norway'},
+		{name:'Hege',country:'Sweden'},
+		{name:'Kai',country:'Denmark'}];
+    
+}
