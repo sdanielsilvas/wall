4b72cd33dc50c01171a40b40a54052630b719016
sdanielsilvas <sdanielsilvas@gmail.com>
2015-01-29 13:36:43 -0500

    primera version del muro

---
+++ publicationController.js
@@ -0,0 +1,8 @@
+function publicationController($scope,$http) {
+ $scope.names = [
+        {name:'que calor!!!',country:'Norway'},
+        {name:'Hola que hacen',country:'Sweden'},
+        {name:'auauauauauauauau',country:'Denmark'}
+    ];
+   	
+}
